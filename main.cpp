1e1529a28def96d807fb37d29c269ee416cefe8e
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-11-21 19:13:23 +0800

    mergeTwoLists.h

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <vector>
+#include "mergeTwoLists.h"
 using std::vector;
 using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
 
-
 int main(){
+    ListNode l13(4);
+    ListNode l12(3, &l13);
+    ListNode l11(1, &l12);
+    ListNode l23(4);
+    ListNode l22(2, &l23);
+    ListNode l21(1, &l22);
+    ListNode *head = mergeTwoLists(&l11,&l21);
+    while (head != nullptr){
+        cout << head->val << endl;
+        head = head->next;
+    }
 }
---
+++ mergeTwoLists.h
@@ -0,0 +1,33 @@
+//
+// Created by Yu4n on 2020-11-18.
+//
+
+#ifndef LEETCODE_IN_CPP_MERGETWOLISTS_H
+#define LEETCODE_IN_CPP_MERGETWOLISTS_H
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+    ListNode dummy;
+    ListNode *tail = &dummy;
+
+    while (l1 && l2) {
+        if (l1->val < l2->val) {
+            tail->next = l1;
+            l1 = l1->next;
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+        }
+        tail = tail->next;
+    }
+
+    tail->next = l1 ? l1 : l2;
+    return dummy.next;
+}
+#endif //LEETCODE_IN_CPP_MERGETWOLISTS_H
