122f12cbdd9bc44780d97087bd5fa615908b2d3d
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-11-30 21:31:06 +0800

    Explanation for the problem

--- find_disappeared_numbers.h
+++ find_disappeared_numbers.h
@@ -6,21 +6,21 @@
 #define LEETCODE_IN_CPP_FIND_DISAPPEARED_NUMBERS_H
 #include <vector>
 #include <cmath>
+using std::vector;
 
 // Firstly, It is very intuitive to come up with
 // using O(N) space to store the information.
-// e.g. [ 4, 3, 2, 7, 8, 2, 3, 1] we can use
-// a binary array (O(N) space) to count it. [ 1, 1, 1, 1, 0, 0, 1, 1]
-// Note that it has the same amount of information with [ -, -, -, -, +, +, -, -].
+// e.g. [4, 3, 2, 7, 8, 2, 3, 1] we can use
+// a binary array (O(N) space) to count it. [1, 1, 1, 1, 0, 0, 1, 1]
+// Note that it has the same amount of information with [-, -, -, -, +, +, -, -].
 // you can just attach these information on the original array
 // without generating a new array. That's the whole point.
 
-using std::vector;
 vector<int> findDisappearedNumbers(vector<int>& nums) {
     int len = nums.size();
     for(int i=0; i<len; i++) {
         int m = abs(nums[i])-1; // index start from 0
-        nums[m] = nums[m]>0 ? -nums[m] : nums[m];
+        nums[m] = nums[m] > 0 ? -nums[m] : nums[m];
     }
     vector<int> res;
     for(int i = 0; i<len; i++) {
@@ -28,4 +28,5 @@ vector<int> findDisappearedNumbers(vector<int>& nums) {
     }
     return res;
 }
+
 #endif //LEETCODE_IN_CPP_FIND_DISAPPEARED_NUMBERS_H
