6d5f3c33725fcf9580c21e98e64659768fde51e6
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-11-30 16:48:57 +0800

    冒泡排序 Accepted (cout << endl; missed).

--- main.cpp
+++ main.cpp
@@ -9,34 +9,28 @@ template <class T> void swap ( T& a, T& b )
 {
     T c(a); a=b; b=c;
 }
-int main(){
-    int m;
-    int *num;
-    int **a;
-    cin >> m;
-    a = new int *[m];
-    num = new int [m];
-    for (int i = 0; i < m; ++i) {
-        cin >> num[i];
-        a[i] = new int[num[i]];
-        for(int j = 0; j<num[i]; ++j){
-            cin >> a[i][j];
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i = 0; i != n; ++i){
+        int j;
+        cin >> j;
+        int num;
+        vector<int> nums;
+        for(int a = 0; a != j; ++a){
+            cin >> num;
+            nums.push_back(num);
         }
-        for (int j = 0; j < num[i] - 1; j++)
-        {
-            if (a[i][j] > a[i][j + 1])
-            {
-                swap(a[i][j], a[i][j + 1]);
-            }
+        for(int k = 0; k != nums.size() - 1; ++k){
+            if(nums[k] > nums[k+1])
+                swap(nums[k],nums[k+1]);
         }
-    }
-    for (int i = 0; i < m; ++i) {
-        for (int j = 0; j < num[i]; ++j) {
-            cout << a[i][j] << ' ';
+        for(auto x : nums){
+            cout << x << ' '; //(it != nums.end() - 1 ? " " : "");
         }
         cout << endl;
+        // 7 3 6 4 7 6 8 2
+        // 8 2 7 5 8 4 9 6 3
     }
-    return 0;
-    //7 3 6 4 7 6 8 2
-    // 8 2 7 5 8 4 9 6 3
 }
