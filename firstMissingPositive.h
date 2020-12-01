8113abc4fafe8e61c873431dbd86067180033d99
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-01 10:15:15 +0800

    find missing positive

---
+++ firstMissingPositive.h
@@ -0,0 +1,19 @@
+//
+// Created by Yu4n on 2020-12-01.
+//
+
+#ifndef LEETCODE_IN_CPP_FIRSTMISSINGPOSITIVE_H
+#define LEETCODE_IN_CPP_FIRSTMISSINGPOSITIVE_H
+#include <vector>
+using std::vector;
+int firstMissingPositive(vector<int>& nums) {
+    int n = nums.size();
+    for (int i = 0; i < n; i++)
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+            std::swap(nums[i], nums[nums[i] - 1]);
+    for (int i = 0; i < n; i++)
+        if (nums[i] != i + 1)
+            return i + 1;
+    return n + 1;
+}
+#endif //LEETCODE_IN_CPP_FIRSTMISSINGPOSITIVE_H
