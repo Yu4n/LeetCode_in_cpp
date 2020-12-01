ced7e99f30a9284da83e67ea696e56b384ea5cc1
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-01 20:05:53 +0800

    拦截导弹1 http://47.99.179.148/problem.php?id=1009

--- main.cpp
+++ main.cpp
@@ -1,48 +1,35 @@
-#include <string.h>
+#include<cstdio>
 #include <iostream>
-#include <vector>
-using std::vector;
-using std::string;
-using std::cout;
-using std::cin;
-using std::endl;
-
-template <class T> void check_and_print(T &vec){
-    cout << "Size: " << vec.size() << "\nContent: [";
-    for (auto it = vec.begin(); it != vec.end(); ++it)
-        cout << *it << (it != vec.end() - 1 ? "," : "");
-    cout << "]\n";
+int max(int a,int b){
+    return a>b ? a : b;
 }
-
+int list[26];
+int dp[26];
 int main(){
-    int cnt;
-    cin >> cnt;
-    for(int c=0;c!=cnt;++c) {
-        int M, N;
-        cin >> N >> M;//M是乘号个数， N是字符串长度
-        char str[N + 1];
-        cin >> str;
-        int dp[N + 1][M + 1];
-        int data[N + 1][N + 1];
-        memset(dp, 0, sizeof(dp));
-        for (int i = 0; i < N; i++) {
-            int jojo = 0;
-            for (int j = i; j < N; j++) {
-                jojo = jojo * 10 + str[j] - '0';
-                data[i][j] = jojo;
+    int ln;
+    std::cin >> ln;
+    for(int i=0;i!=ln;++i) {
+        int n;
+        while (scanf("%d", &n) != EOF) {
+            for (int i = 1; i <= n; i++) {
+                scanf("%d", &list[i]);
             }
-        }
-        for (int i = 0; i < N; i++) {
-            dp[i][0] = data[0][i];
-        }
-        for (int i = 0; i < N; i++) {
-            for (int j = 1; j <= M; j++) {
-                for (int k = 0; k < i; k++) {
-                    dp[i][j] = std::max(dp[k][j - 1] * data[k + 1][i], dp[i][j]);
+            for (int i = 1; i <= n; i++) {
+                int tmax = 1;
+                for (int j = 1; j < i; j++) {
+                    if (list[j] >= list[i]) {
+                        tmax = max(tmax, dp[j] + 1);
+                    }
                 }
+                dp[i] = tmax;
             }
+            int ans = 1;
+            for (int i = 1; i <= n; i++) {
+                ans = max(ans, dp[i]);
+            }
+            printf("%d\n", ans);
         }
-        cout << dp[N - 1][M] << endl;
     }
     return 0;
 }
+// 7 300 250 275 252 200 138 245
