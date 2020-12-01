2a4d577a310a2dac704e1da929d6083f1348d327
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-01 09:13:31 +0800

    findDuplicates.h

---
+++ findDuplicates.h
@@ -0,0 +1,20 @@
+//
+// Created by Yu4n on 2020-11-30.
+//
+
+#ifndef LEETCODE_IN_CPP_FINDDUPLICATES_H
+#define LEETCODE_IN_CPP_FINDDUPLICATES_H
+#include <vector>
+using std::vector;
+// 4,3,2,7,8,2,3,1
+vector<int> findDuplicates(vector<int>& nums){
+    vector<int> res;
+    for(int i=0;i!=nums.size();i++){
+        int m = abs(nums[i]) - 1;
+        nums[m] = -nums[m];
+        if(nums[m] > 0)
+            res.push_back(abs(nums[i]));
+    }
+    return res;
+}
+#endif //LEETCODE_IN_CPP_FINDDUPLICATES_H
