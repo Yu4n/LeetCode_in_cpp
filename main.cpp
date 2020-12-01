264ac0f884c3a6068f4b5afd4ba4a4c75e35ebbe
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-01 19:43:04 +0800

    最大乘积 http://47.99.179.148/problem.php?id=1007

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <string.h>
 #include <iostream>
 #include <vector>
 using std::vector;
@@ -12,10 +13,36 @@ template <class T> void check_and_print(T &vec){
         cout << *it << (it != vec.end() - 1 ? "," : "");
     cout << "]\n";
 }
-int main()
-{
-    vector<int> nums{4,3,2,7,8,2,3,1};
-    vector<int> res;
-    check_and_print(res);
+
+int main(){
+    int cnt;
+    cin >> cnt;
+    for(int c=0;c!=cnt;++c) {
+        int M, N;
+        cin >> N >> M;//M是乘号个数， N是字符串长度
+        char str[N + 1];
+        cin >> str;
+        int dp[N + 1][M + 1];
+        int data[N + 1][N + 1];
+        memset(dp, 0, sizeof(dp));
+        for (int i = 0; i < N; i++) {
+            int jojo = 0;
+            for (int j = i; j < N; j++) {
+                jojo = jojo * 10 + str[j] - '0';
+                data[i][j] = jojo;
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            dp[i][0] = data[0][i];
+        }
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j <= M; j++) {
+                for (int k = 0; k < i; k++) {
+                    dp[i][j] = std::max(dp[k][j - 1] * data[k + 1][i], dp[i][j]);
+                }
+            }
+        }
+        cout << dp[N - 1][M] << endl;
+    }
     return 0;
 }
