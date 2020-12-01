ffccafba06c2672a6debae3b6d3a58ed24e8bd5f
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-01 09:13:43 +0800

    findDuplicates.h

--- main.cpp
+++ main.cpp
@@ -7,13 +7,17 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+template <class T> void check_and_print(T &vec){
+    cout << "Size: " << vec.size() << "\nContent: [";
+    for (auto it = vec.begin(); it != vec.end(); ++it)
+        cout << *it << (it != vec.end() - 1 ? "," : "");
+    cout << "]\n";
+}
 int main()
 {
     vector<int> nums{4,3,2,7,8,2,3,1};
     vector<int> res;
-    res = findDisappearedNumbers(nums);
-    for(auto x:res){
-        cout << x << ' ';
-    }
+    res = findDuplicates(nums);
+    check_and_print(res);
     return 0;
 }
