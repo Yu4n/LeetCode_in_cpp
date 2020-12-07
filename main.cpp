8820b0da38b51de75a4bbb15360cc4789b6eb234
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-07 20:34:05 +0800

    Wrong! Answer for symmetric balanced string!

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 using std::vector;
 using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
 #include "balancedStringSplit.h"
-#include "check&print.h"
 
 int main () {
-    string s = "ewfewf";
-    check_and_print(s);
-    int i = balancedStringSplit(s);
+    string s = "RLRRLLRLRL";
+    cout << balancedStringSplit(s);
     return 0;
 }
