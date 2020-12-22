eecd57032cfccb329e27905e5214fdb8efec9cf1
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-22 20:27:04 +0800

    countBits.h

--- countBits.h
+++ countBits.h
@@ -4,7 +4,11 @@
 
 #ifndef LEETCODE_IN_CPP_COUNTBITS_H
 #define LEETCODE_IN_CPP_COUNTBITS_H
+// https://leetcode.com/problems/counting-bits/discuss/79527/Four-lines-C%2B%2B-time-O(n)-space-O(n)
 vector<int> countBits(int num) {
-
+    vector<int> ret(num+1, 0);
+    for (int i = 1; i <= num; ++i)
+        ret[i] = ret[i&(i-1)] + 1;
+    return ret;
 }
 #endif //LEETCODE_IN_CPP_COUNTBITS_H
