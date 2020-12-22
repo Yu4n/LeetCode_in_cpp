86a38e070dc0024ead21e8b6f2d191ff2fc5cf27
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-22 20:29:49 +0800

    countBits.h

--- countBits.h
+++ countBits.h
@@ -11,4 +11,10 @@ vector<int> countBits(int num) {
         ret[i] = ret[i&(i-1)] + 1;
     return ret;
 }
+
+// i&(i-1) drops the lowest set bit. For example: i = 14,
+// its binary representation is 1110, so i-1 is 1101, i&(i-1) = 1100,
+// the number of "1" in its binary representation decrease one,
+// so ret[i] = ret[i&(i-1)] + 1.
+
 #endif //LEETCODE_IN_CPP_COUNTBITS_H
