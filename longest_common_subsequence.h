507f853383f89c6c132f9004b7aee1ea0ff50ab8
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-23 15:16:43 +0800

    partition_equal_subset_sum.h

--- longest_common_subsequence.h
+++ longest_common_subsequence.h
@@ -4,7 +4,7 @@
 
 #ifndef LEETCODE_IN_CPP_LONGEST_COMMON_SUBSEQUENCE_H
 #define LEETCODE_IN_CPP_LONGEST_COMMON_SUBSEQUENCE_H
-
+// https://leetcode.com/problems/longest-common-subsequence/discuss/348884/C%2B%2B-with-picture-O(nm)
 int longestCommonSubsequence(string text1, string text2) {
     int m = text1.size(), n = text2.size();
     if(m < n) return longestCommonSubsequence(text2, text1);
