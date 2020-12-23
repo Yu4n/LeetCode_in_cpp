49045ffe1e0b70432e0c7128a42e1c195b552ec3
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-23 20:53:27 +0800

    0-1背包问题1 by myself

--- main.cpp
+++ main.cpp
@@ -1,40 +1,42 @@
 #include <iostream>
+#include <vector>
+using std::vector;
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
 
-int m,n;
-int capacity;
-int sizes[501];
-int values[501];
-int dp[501][101];
-int max(int a,int b){
-    return a>=b?a:b;
-}
-int kn(int n,int c,int sizes[], int values[]){
-    for(int i =1;i<=n;++i){
-        for (int j = 1; j <= c; ++j)
-        {
-            if (sizes[i] > j) // 当前背包的容量 j 放不下第 i 件商品时
-            {
-                dp[i][j] = dp[i-1][j]; // 放弃第 i 件商品，拿第 i - 1 件商品
-            }
-            else
-            {
-                dp[i][j] = max(dp[i-1][j-sizes[i]]+values[i],dp[i-1][j]);
+int knapsack(int num, int cap, int wt[], int val[]){
+    int dp[num+1][cap+1];
+    for(int i = 0; i <= cap; ++i){
+        dp[0][i] = 0;
+    }
+    for(int i = 0; i <= num; ++i){
+        dp[i][0] = 0;
+    }
+    for(int i = 1; i <= num; ++i){
+        for(int j = 1; j <= cap; ++j){
+            if (wt[i-1] > j){
+                dp[i][j] = dp[i-1][j];
+            } else{
+                dp[i][j] = std::max(dp[i-1][j],val[i-1]+dp[i-1][j-wt[i-1]]);
             }
         }
     }
-    return dp[n][c];
+    return dp[num][cap];
 }
 int main() {
-    int i,j;
-    int v;
-    std::cin >> m;
-    for(i=0; i<m; ++i){
-        std::cin >> n >> capacity;
-        for(j=1; j<=n; ++j){
-            std::cin >> sizes[j] >> values[j];
+
+    int times;
+    cin >> times;
+    for (int t = 0; t < times; ++t){
+        int num, cap;
+        cin >> num >> cap;
+        int wt[num], val[num];
+        for(int nu = 0; nu < num; ++nu){
+            cin >> wt[nu] >> val[nu];
         }
-        v = kn(n,capacity,sizes,values);
-        std::cout << v << std::endl;
+        cout << knapsack(num, cap, wt, val) << endl;
     }
     return 0;
 }
