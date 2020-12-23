171d1c72aa179109fa271a2c0604686572e4ee31
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-23 22:44:50 +0800

    0-1背包问题 by myself

--- main.cpp
+++ main.cpp
@@ -6,25 +6,41 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-int knapsack(int num, int cap, int wt[], int val[]){
+int knapsack_full(int num, int cap, int wt[], int val[]){
     int dp[num+1][cap+1];
-    for(int i = 0; i <= cap; ++i){
-        dp[0][i] = 0;
-    }
-    for(int i = 0; i <= num; ++i){
+    for (int j = 0; j <= num; ++j)
+        for(int i = 1; i <= cap; ++i)
+            dp[j][i] = -1;
+
+    for (int i = 0; i <= num; ++i)
         dp[i][0] = 0;
-    }
+
     for(int i = 1; i <= num; ++i){
         for(int j = 1; j <= cap; ++j){
             if (wt[i-1] > j){
                 dp[i][j] = dp[i-1][j];
             } else{
-                dp[i][j] = std::max(dp[i-1][j],val[i-1]+dp[i-1][j-wt[i-1]]);
+                int prior = dp[i-1][j-wt[i-1]];
+                int temp;
+                if (prior != -1){
+                    temp = val[i-1]+dp[i-1][j-wt[i-1]];
+                } else{
+                    temp = -1;
+                }
+                dp[i][j] = std::max(temp,dp[i-1][j]);
+//                if (dp[i-1][j] != -1)
+//                    dp[i][j] = std::max(dp[i-1][j],val[i-1]+dp[i-1][j-wt[i-1]]);
+//                else
+//                    dp[i][j] = dp[i-1][j];
             }
         }
     }
-    return dp[num][cap];
+    if (dp[num][cap] == -1)
+        return 0;
+    else
+        return dp[num][cap];
 }
+
 int main() {
     int times;
     cin >> times;
@@ -35,7 +51,9 @@ int main() {
         for(int nu = 0; nu < num; ++nu){
             cin >> wt[nu] >> val[nu];
         }
-        cout << knapsack(num, cap, wt, val) << endl;
+        cout << knapsack_full(num, cap, wt, val) << endl;
     }
+//    int wt[] = {1,2,6}, val[] = {3,5,2};
+//    cout << knapsack_full(5,4,wt,val);
     return 0;
 }
