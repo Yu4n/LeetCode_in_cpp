2f3a30976a2c1d6fe23702857a1e5941c639816b
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-23 09:09:54 +0800

    longest_common_subsequence.h my own code

--- longest_common_subsequence.h
+++ longest_common_subsequence.h
@@ -8,17 +8,18 @@
 int longestCommonSubsequence(string text1, string text2) {
     int m = text1.size(), n = text2.size();
     if(m < n) return longestCommonSubsequence(text2, text1);
-    vector<vector<int>> memo(2, vector<int>(n + 1));
-    for(int i = 1; i < m + 1; i++){
-        for(int j = 1; j < n + 1; j++){
-            if(text1[i-1] == text2[j-1]){
-                memo[i % 2][j] = 1 + memo[(i - 1) % 2][j - 1];
+    vector<vector<int>> memo(n + 1, vector<int>(m+1));
+    for (int i = 1; i <= n; ++i){
+        for (int j = 1; j <= m; ++j){
+            if (text2[i-1] == text1[j-1]){
+                memo[i][j] = memo[i-1][j-1] + 1;
             }
-            else
-                memo[i % 2][j] = max(memo[(i - 1) % 2][j], memo[i % 2][j - 1]);
+            else{
+                memo[i][j] = std::max(memo[i-1][j],memo[i][j-1]);
+            }
         }
     }
-    return memo[m % 2][n];
+    return memo[n][m];
 }
 
 #endif //LEETCODE_IN_CPP_LONGEST_COMMON_SUBSEQUENCE_H
