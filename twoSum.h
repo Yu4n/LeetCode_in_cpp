c78f13c0fa7dd92229ba2c06ea4508ef5839f72e
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-24 10:18:00 +0800

    twoSum.h

--- twoSum.h
+++ twoSum.h
@@ -18,4 +18,22 @@ std::vector<int> twoSum(std::vector<int>& nums, int target) {
         imap[nums[i]] = i;
     }
 }
+vector<int> twoSum1(vector<int>& nums, int target) {
+    vector<int> temp;
+    bool flag = false;
+    for(int i=1; i<nums.size(); i++){
+        for(int j = 0; j<i; j++){
+            if(nums.at(i)+nums.at(j)==target){
+                temp.push_back(i);
+                temp.push_back(j);
+                flag = true;
+                break;
+            }
+        }
+        if(flag){
+            break;
+        }
+    }
+    return temp;
+}
 #endif //LEETCODE_IN_CPP_TWOSUM_H
