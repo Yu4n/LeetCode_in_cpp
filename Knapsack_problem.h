28ad3704950d1f1b03b15f13f56c68f70d4ad605
Yu4n <28648731+Yu4n@users.noreply.github.com>
2020-12-24 08:58:53 +0800

    Knapsack_problem.h

--- Knapsack_problem.h
+++ Knapsack_problem.h
@@ -66,5 +66,32 @@ int knapsack(int num, int cap, int wt[], int val[]){
     }
     return dp[num][cap];
 }
+int knapsack_full(int num, int cap, int wt[], int val[]){
+    int dp[num+1][cap+1];
+    for (int j = 0; j <= num; ++j)
+        for(int i = 1; i <= cap; ++i)
+            dp[j][i] = -1;
 
+    for (int i = 0; i <= num; ++i)
+        dp[i][0] = 0;
+
+    for(int i = 1; i <= num; ++i){
+        for(int j = 1; j <= cap; ++j){
+            if (wt[i-1] > j){
+                dp[i][j] = dp[i-1][j];
+            } else{
+                int prior = dp[i-1][j-wt[i-1]];
+                if (prior != -1){
+                    dp[i][j] = std::max(prior + val[i-1], dp[i-1][j]);
+                } else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+    }
+    if (dp[num][cap] == -1)
+        return 0;
+    else
+        return dp[num][cap];
+}
 #endif //LEETCODE_IN_CPP_KNAPSACK_PROBLEM_H
