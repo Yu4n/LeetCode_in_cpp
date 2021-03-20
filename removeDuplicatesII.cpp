54a8dffa5841b7097379b44487f79c950a62f911
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-03-20 21:58:08 +0800

    removeDuplicatesII.cpp with two pointers in 6 lines

--- removeDuplicatesII.cpp
+++ removeDuplicatesII.cpp
@@ -3,23 +3,9 @@
 //
 
 int removeDuplicates(vector<int>& nums) {
-    if(nums.empty())
-        return 0;
-    int tmp = nums[0], cnt = 1;
-    for(auto it = nums.begin() + 1; it != nums.end(); ){
-        if(tmp == *it){
-            ++cnt;
-            ++it;
-        }
-        else{
-            tmp = *it;
-            cnt = 1;
-            ++it;
-        }
-        if(cnt > 2){
-            it = nums.erase(it-cnt+2, it);
-            cnt--;
-        }
-    }
-    return nums.size();
+    int i = 0;
+    for (int n : nums)
+        if (i < 2 || n > nums[i-2])
+            nums[i++] = n;
+    return i;
 }
