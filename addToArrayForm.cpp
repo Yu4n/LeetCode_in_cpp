d86241d26fad3cfe895c52252c672ac0684e010e
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-03-22 17:36:52 +0800

    addToArrayForm.cpp

---
+++ addToArrayForm.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Yu4n on 2021-03-22.
+//
+
+class Solution {
+public:
+    vector<int> addToArrayForm(vector<int> &A, int K)
+    {
+        int i, size = A.size();
+
+        for (i = size - 1; i >= 0 && K != 0; i--)
+        {
+            K = K + A[i];
+            A[i] = K % 10;
+            K = K / 10;
+        }
+        while (K != 0)
+        {
+            A.insert(A.begin(), K % 10);
+            K = K / 10;
+        }
+        return A;
+    }
+};
