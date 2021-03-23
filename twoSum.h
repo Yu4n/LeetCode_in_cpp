9c49a49ef3663d6aa73ba94bae0bb4b4c0ce8576
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-03-23 10:36:55 +0800

    twoSum.h using unordered_map

--- twoSum.h
+++ twoSum.h
@@ -4,20 +4,16 @@
 #ifndef LEETCODE_IN_CPP_TWOSUM_H
 #define LEETCODE_IN_CPP_TWOSUM_H
 
-// #include <unordered_map>
-
-std::vector<int> twoSum(std::vector<int>& nums, int target) {
-    std::unordered_map<int, int> imap;
-
-    for (int i = 0;; ++i) {
-        auto it = imap.find(target - nums[i]);
-
-        if (it != imap.end())
-            return std::vector<int> {i, it->second};
-
-        imap[nums[i]] = i;
+vector<int> twoSum(vector<int>& nums, int target) {
+    std::unordered_map<int,int> mp;
+    for(int i = 0; ; ++i){
+        if(mp.find(target - nums[i]) != mp.end()){
+            return vector<int> {i, mp.find(target - nums[i])->second};
+        }
+        mp[nums[i]] = i;
     }
 }
+
 vector<int> twoSum1(vector<int>& nums, int target) {
     vector<int> temp;
     bool flag = false;
