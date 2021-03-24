df582abc0bcb24d3bb2eaff2e8db6a5946534106
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-03-24 20:20:45 +0800

    searchInsert.cpp using binary search

--- searchInsert.cpp
+++ searchInsert.cpp
@@ -10,3 +10,16 @@ int searchInsert(vector<int>& nums, int target) {
             return i;
     return nums.size();
 }
+
+int BinarySearchInsert(vector<int>& nums, int target) {
+    int l = 0, h = nums.size() - 1, mid;
+    while (l <= h){
+        mid = l + (h-l)/2;
+        if (target <= nums[mid]){
+            h = mid - 1;
+        } else{
+            l = mid + 1;
+        }
+    }
+    return l;
+}
