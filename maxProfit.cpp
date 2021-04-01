0580485896ff02e717eeb97a2fa9024d15b7f9ad
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-04-01 17:24:41 +0800

    maxProfit.cpp hard DP

--- maxProfit.cpp
+++ maxProfit.cpp
@@ -23,3 +23,17 @@ int maxProfitII(vector<int>& prices) {
     }
     return maxProfit;
 }
+
+int maxProfitIII(vector<int>& prices) {
+    if(prices.size()==1)
+        return 0;
+    ios::sync_with_stdio(0);
+    int t1Cost = INT_MAX, t2Cost = INT_MAX,t1Profit = 0, t2Profit = 0;
+    for (int price : prices) {
+        t1Cost = min(t1Cost, price); // the maximum profit if only one transaction is allowed
+        t1Profit = max(t1Profit, price - t1Cost);
+        t2Cost = min(t2Cost, price - t1Profit); // re-invest profit in the second transaction
+        t2Profit = max(t2Profit, price - t2Cost);
+    }
+    return t2Profit;
+}
