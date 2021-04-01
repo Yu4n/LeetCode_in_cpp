9902806747fa10d44b9f01be79ec23888c15d1f8
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-04-01 21:05:45 +0800

    isHappy.cpp

---
+++ isHappy.cpp
@@ -0,0 +1,26 @@
+//
+// Created by Yu4n on 2021-04-01.
+//
+
+int digitSquareSum(int n) {
+    int sum = 0, tmp;
+    while (n) {
+        tmp = n % 10;
+        sum += tmp * tmp;
+        n /= 10;
+    }
+    return sum;
+}
+
+// Cycle detection by fast and slow https://en.wikipedia.org/wiki/Cycle_detection
+bool isHappy(int n) {
+    int slow, fast;
+    slow = fast = n;
+    do {
+        slow = digitSquareSum(slow);
+        fast = digitSquareSum(fast);
+        fast = digitSquareSum(fast);
+        if(fast == 1) return 1;
+    } while(slow != fast);
+    return 0;
+}
