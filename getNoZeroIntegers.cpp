d2844d743bc5ffddfbe1670f6ba2884b4018aa34
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-04-04 00:42:21 +0800

    getNoZeroIntegers.cpp quicker solution

--- getNoZeroIntegers.cpp
+++ getNoZeroIntegers.cpp
@@ -24,3 +24,24 @@ vector<int> getNoZeroIntegers(int n) {
     }
     return {i,n-i};
 }
+
+vector<int> getNoZeroIntegersI(int n) {
+    int a=0, b=0, step=1;
+
+    while (n>0) {
+        int d = n % 10; // digit
+        n /= 10;
+
+        if ((d == 0 || d == 1) && n>0) { // n>0 evades the case when 1 is the most significant digit
+            a += step*(1+d);
+            b += step*9;
+            n--; // handle carry
+        } else {
+            a += step*1;
+            b += step*(d-1);
+        }
+        step *= 10;
+    }
+
+    return {a,b};
+}
