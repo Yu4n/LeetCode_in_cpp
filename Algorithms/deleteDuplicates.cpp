9b6406f41bf080285b92f504624d2759807061f6
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-04-06 22:59:00 +0800

    deleteDuplicates.cpp II medium

--- Algorithms/deleteDuplicates.cpp
+++ Algorithms/deleteDuplicates.cpp
@@ -11,3 +11,23 @@ ListNode* deleteDuplicates(ListNode* head) {
             head = head->next;
     return dummy;
 }
+
+ListNode* deleteDuplicatesII(ListNode* curr) {
+    if (!curr){
+        return nullptr;
+    }
+    ListNode *sentinel = new ListNode(0, curr);
+    auto prev = sentinel;
+    while (curr){
+        if (curr->next && curr->val == curr->next->val){
+            while (curr->next && curr->val == curr->next->val){
+                curr = curr->next;
+            }
+            prev->next = curr->next;
+        } else{
+            prev = prev->next;
+        }
+        curr = curr->next;
+    }
+    return sentinel->next;
+}
