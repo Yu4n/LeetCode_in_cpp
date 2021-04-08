6dc669ca2eba3859de7a915209565562462c5e30
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-04-08 12:46:54 +0800

    sortedArrayToBST.cpp

---
+++ Algorithms/sortedArrayToBST.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Yu4n on 2021-04-08.
+//
+
+TreeNode* build(vector<int>& nums, int p1, int p2) {
+    if (p1 > p2) return nullptr;
+    if (p1 == p2) return new TreeNode(nums[p1]);
+    int p0 = (p1+p2)/2;
+    TreeNode* node = new TreeNode(nums[p0]);
+    node->left = build(nums, p1, p0-1);
+    node->right = build(nums, p0+1, p2);
+    return node;
+}
+
+TreeNode* sortedArrayToBST(vector<int>& nums) {
+    return build(nums, 0, nums.size()-1);
+}
