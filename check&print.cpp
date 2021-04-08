0ae7d321767108d039361a194b4231883321b34f
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-04-08 17:21:44 +0800

    update nodes info

---
+++ check&print.cpp
@@ -0,0 +1,45 @@
+//
+// Created by Yu4n on 2020-12-01.
+//
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+
+template <class T> void check_and_print(T &vec){
+    cout << "Size: " << vec.size() << "\nContent: [";
+    for (auto it = vec.begin(); it != vec.end(); ++it)
+        cout << *it << (it != vec.end() - 1 ? "," : "");
+    cout << "]\n";
+}
