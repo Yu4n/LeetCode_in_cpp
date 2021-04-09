9fb86d7846a439a0edc928f5781789a3e08dd879
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-04-09 20:31:20 +0800

    isBalanced.cpp iterative

--- Algorithms/isBalanced.cpp
+++ Algorithms/isBalanced.cpp
@@ -15,3 +15,37 @@ bool isBalanced(TreeNode *root) {
     int right = depth(root->right);
     return abs(left - right) <= 1 && isBalanced(root->left) && isBalanced(root->right);
 }
+
+class Solution {
+public:
+    bool isBalanced(TreeNode *root) {
+        if (!root) return true;
+        TreeNode *cur = root;
+        TreeNode *prev = nullptr;
+        while (cur || !s.empty()) {
+            if (cur) {
+                s.push(cur);
+                cur = cur->left;
+            } else {
+                cur = s.top();
+
+                if (!cur->right || prev == cur->right) {
+                    int left = (m.find(cur->left) != m.end()) ? m[cur->left] : 0;
+                    int right = (m.find(cur->right) != m.end()) ? m[cur->right] : 0;
+                    if (abs(left - right) > 1) return false;
+                    m[cur] = 1 + max(left, right);
+                    prev = cur;
+                    cur = nullptr;
+                    s.pop();
+                } else {
+                    cur = cur->right;
+                }
+            }
+        }
+        return true;
+    }
+
+private:
+    stack<TreeNode *> s;
+    unordered_map<TreeNode *, int> m;
+};
