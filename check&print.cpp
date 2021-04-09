53ab12737872382d9e9be4140198d15b6af5ea00
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-04-09 21:06:27 +0800

    update

--- check&print.cpp
+++ check&print.cpp
@@ -1,15 +1,22 @@
 //
 // Created by Yu4n on 2020-12-01.
 //
+#include <iostream>
+#include <vector>
+
 using std::vector;
 using std::string;
 using std::cout;
 using std::endl;
+
 struct ListNode {
     int val;
     ListNode *next;
+
     ListNode() : val(0), next(nullptr) {}
+
     ListNode(int x) : val(x), next(nullptr) {}
+
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
@@ -17,30 +24,37 @@ struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
+
     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
 class Node {
 public:
     int val;
-    vector<Node*> neighbors;
+    vector<Node *> neighbors;
+
     Node() {
         val = 0;
-        neighbors = vector<Node*>();
+        neighbors = vector<Node *>();
     }
+
     Node(int _val) {
         val = _val;
-        neighbors = vector<Node*>();
+        neighbors = vector<Node *>();
     }
-    Node(int _val, vector<Node*> _neighbors) {
+
+    Node(int _val, vector<Node *> _neighbors) {
         val = _val;
         neighbors = _neighbors;
     }
 };
 
-template <class T> void check_and_print(T &vec){
+template<class T>
+void check_and_print(T &vec) {
     cout << "Size: " << vec.size() << "\nContent: [";
     for (auto it = vec.begin(); it != vec.end(); ++it)
         cout << *it << (it != vec.end() - 1 ? "," : "");
