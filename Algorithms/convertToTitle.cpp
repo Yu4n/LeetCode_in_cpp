6d51963b9ca1a1f5030cd3d8a982e71608177792
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-04-10 13:34:02 +0800

    convertToTitle.cpp

--- Algorithms/convertToTitle.cpp
+++ Algorithms/convertToTitle.cpp
@@ -5,14 +5,11 @@
 string convertToTitle(int target) {
     int tmp;
     string res;
-    while (target > 0) {
+    while (target) {
+        target -= 1;
         tmp = target % 26;
         target = target / 26;
-        if(tmp == 0){
-            tmp = 26;
-            target -= 1;
-        }
-        res.push_back(char(tmp + 64));
+        res.push_back(tmp + 65);
     }
     std::reverse(res.begin(), res.end());
     return res;
