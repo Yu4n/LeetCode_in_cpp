fd98a443b5f31ba5e6486a1bd83d21e6a07e3e88
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-04-10 17:44:13 +0800

    hammingWeight.cpp

---
+++ Algorithms/hammingWeight.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Yu4n on 2021-04-10.
+//
+
+int hammingWeight(uint32_t n) {
+    int res = 0;
+    for (int i = 0; i < 32; ++i) {
+        if (n & 1) {
+            res += 1;
+        }
+        n >>= 1;
+    }
+    return res;
+}
