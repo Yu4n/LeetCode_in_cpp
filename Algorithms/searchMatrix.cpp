0c43c82b8e009a01b622bf9449c3243dce680ef5
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-04-15 22:30:52 +0800

    searchMatrix.cpp

---
+++ Algorithms/searchMatrix.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Yu4n on 2021-04-15.
+//
+
+bool searchMatrix(vector <vector<int>> &matrix, int target) {
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int l = 0, r = m * n - 1;
+    while (l != r) {
+        int mid = (l + r - 1) >> 1;
+        if (matrix[mid / m][mid % m] < target)
+            l = mid + 1;
+        else
+            r = mid;
+    }
+    return matrix[r / m][r % m] == target;
+}
