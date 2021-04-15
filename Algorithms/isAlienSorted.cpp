ab594ec7c3808076a338ab8ab6e920298db27f2f
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-04-15 09:52:19 +0800

    isAlienSorted.cpp

---
+++ Algorithms/isAlienSorted.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Yu4n on 2021-04-13.
+//
+
+bool isAlienSorted(vector<string> words, string order) {
+    int mapping[26];
+    for (int i = 0; i < 26; i++)
+        mapping[order[i] - 'a'] = i;
+    for (string &w : words)
+        for (char &c : w)
+            c = mapping[c - 'a'];
+    return is_sorted(words.begin(), words.end());
+}
