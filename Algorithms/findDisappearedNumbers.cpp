8fc5fabb06a2e8198d3505ec85c765f82a1693ed
Yu4n <28648731+Yu4n@users.noreply.github.com>
2021-04-15 12:41:03 +0800

    findDisappearedNumbers.cpp

--- Algorithms/findDisappearedNumbers.cpp
+++ Algorithms/findDisappearedNumbers.cpp
@@ -22,3 +22,16 @@ vector<int> findDisappearedNumbers(vector<int> &nums) {
     }
     return res;
 }
+
+vector<int> findDisappearedNumbers(vector<int>& nums) {
+    int len = nums.size();
+    vector<int> res;
+    for(int i=0; i<len; i++) {
+        int m = abs(nums[i])-1; // index start from 0
+        nums[m] = nums[m] > 0 ? -nums[m] : nums[m];
+    }
+    for(int i = 0; i<len; i++) {
+        if(nums[i] > 0) res.push_back(i+1);
+    }
+    return res;
+}
